e021b1cac1a93ce7d5752725c401bbb72b95d678
HenriqueContini <henrique.coutine@gmail.com>
2023-06-21 17:29:32 -0300

    Chore: New banner design

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,6 +4,14 @@ export default function Banner() {
   return (
     <section className={styles.banner__container}>
       <img className={styles.banner__img} src="https://www.freetogame.com/g/540/thumbnail.jpg" alt="Banner" />
+      <article className={styles.banner__article}>
+        <h2 className={styles.banner__title}>Overwatch 2</h2>
+        <p className={styles.banner__text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod perspiciatis nihil ipsam nostrum quaerat quia
+          dolore facilis a magnam voluptatibus! Tempora harum eum autem temporibus,
+          nihil consequuntur sequi iure pariatur.
+        </p>
+      </article>
     </section>
   )
 }

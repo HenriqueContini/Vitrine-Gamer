ad6a7653f8e6efd608b83e55778b6bcc7039068a
HenriqueContini <henrique.coutine@gmail.com>
2023-06-21 18:01:35 -0300

    Chore: New icon and logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,10 @@
 import styles from './header.module.css'
-import icon from '../../assets/control-icon.png'
+import logo from '../../assets/logo.png'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.icon__container}>
-        <img src={icon} alt="Ícone Vitrine Gamer" />
-        <h1>Vitrine Gamer</h1>
-      </div>
+      <img className={styles.logo} src={logo} alt="Logo Vitrine Gamer" />
     </header>
   )
 }

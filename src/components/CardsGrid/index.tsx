a28d69d8ff1151766cc6c24448a6af49a4c6c1b9
HenriqueContini <henrique.coutine@gmail.com>
2023-07-07 11:35:34 -0300

    Feat: Cards Grid Infinite Scroll

--- src/components/CardsGrid/index.tsx
+++ src/components/CardsGrid/index.tsx
@@ -1,15 +1,60 @@
+import { useEffect, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroller';
 import Game from '../../interfaces/Game'
 import Card from '../Card'
 import * as S from './styles'
+import Loader from '../Loader';
 
 interface CardsGridProps {
   data: Game[]
 }
 
-export default function CardsGrid({data}: CardsGridProps) {
+export default function CardsGrid({ data }: CardsGridProps) {
+  const [splittedData, setSplittedData] = useState<Game[][]>()
+  const [displayData, setDisplayData] = useState<Game[]>([])
+  const [currentData, setCurrentData] = useState<number>(0)
+
+  const splitData = (len: number) => {
+    let parts = [], i = 0, n = data.length
+
+    while (i < n) {
+      parts.push(data.slice(i, i += len))
+    }
+
+    return parts
+  }
+
+  const loadMoreData = () => {
+    if (splittedData) {
+      let newIndex = currentData + 1
+
+      if (newIndex < splittedData.length) {
+        let newData = splittedData[newIndex]
+        setCurrentData(newIndex)
+
+        setDisplayData(prev => [...prev, ...newData])
+      }
+    }
+  }
+
+  useEffect(() => {
+    setSplittedData(splitData(12))
+    setCurrentData(0)
+  }, [data])
+
+  useEffect(() => {
+    if (splittedData) {
+      setDisplayData(splittedData[0])
+    }
+  }, [splittedData])
+
   return (
     <S.Cards>
-      {data.map(game => <Card key={game.id} title={game.title} thumbnail={game.thumbnail} genre={game.genre} />)}
+      {displayData &&
+        <InfiniteScroll className='scroller' loadMore={() => loadMoreData()} hasMore={splittedData && currentData < splittedData.length} loader={<Loader key={0}/>}>
+          {displayData.map(game => <Card key={game.id} title={game.title} thumbnail={game.thumbnail} genre={game.genre} />)}
+        </InfiniteScroll >
+      }
     </S.Cards>
   )
 }
--- src/components/CardsGrid/styles.ts
+++ src/components/CardsGrid/styles.ts
@@ -1,18 +1,25 @@
 import { styled } from "styled-components";
 
 export const Cards = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
+  & .scroller {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
 
   @media screen and (min-width: 600px) {
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
+    & .scroller {
+
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: space-around;
+    }
   }
 
   @media screen and (min-width: 1080px) {
-    gap: 50px;
-    max-width: 1400px;
+    & .scroller {
+      gap: 50px;
+      max-width: 1400px;
+    }
   }
 `
